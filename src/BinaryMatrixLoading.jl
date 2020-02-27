78101fb4e2f66e9aafeee69737ae4c5a30e0fdd4
jekyllstein <jekyllstein@gmail.com>
2020-02-26 18:52:06 -0700

    Add index reading function and tests

--- src/BinaryMatrixLoading.jl
+++ src/BinaryMatrixLoading.jl
@@ -109,6 +109,42 @@ function read_bin_matrix_col(filename::AbstractString, col::Int64, printdims::Bo
 	v = read!(f, Vector{Float32}(undef, n))
 end
 
-export write_matrix, read_bin_matrix, read_bin_matrix_dims, read_bin_matrix_col
+"""
+	m::Matrix{Float32} = read_bin_matrix_inds(filename::AbstractString, rows::AbstractVector{Int64}, cols::AbstractVector{Int64}, printdims::Bool = false)
+
+Read a submatrix with rows and cols from a Float32 matrix written to file generated with `write_matrix`.
+
+# Examples
+```julia-repl
+julia> using BinaryMatrixLoading
+julia> input = rand(Float32, 10, 10);
+julia> write_matrix(input, "test.bin")
+julia> rows = rand(1:10, 5)
+julia> cols = rand(1:10, 5)
+julia> m = read_bin_matrix_inds("test.bin", rows, cols)
+julia> @assert reduce(&, m .== view(input, rows, cols))
+```
+"""
+function read_bin_matrix_inds(filename::AbstractString, rows::AbstractVector{Int64}, cols::AbstractVector{Int64}, printdims::Bool = false)
+	#read array in binary form from a file.  
+	f = open(filename)
+	#get length of params array
+	n = read(f, Int64)
+	m = read(f, Int64)
+	printdims && println("Got the following array dimensions: $n rows and $m columns")
+	finalout = Matrix{Float32}(undef, length(rows), length(cols))	
+	colout = Vector{Float32}(undef, n)
+	startpos = position(f)
+	for (i, col) in enumerate(cols)
+		nbytes = (col-1)*n*4
+		skip(f, nbytes) 
+		read!(f, colout)
+		view(finalout, :, i) .= view(colout, rows)
+		seek(f, startpos)
+	end
+	return finalout
+end
+
+export write_matrix, read_bin_matrix, read_bin_matrix_dims, read_bin_matrix_col, read_bin_matrix_inds
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,4 +7,15 @@ write_matrix(input, "test.bin")
 input2 = read_bin_matrix("test.bin")
 @test input == input2
 
+(n, m) = read_bin_matrix_dims("test.bin")
+@test size(input) == (n, m)
+
+v = read_bin_matrix_col("test.bin", 5)
+@test v == input[:, 5]
+
+rows = rand(1:10, 5)
+cols = rand(1:10, 5)
+m = read_bin_matrix_inds("test.bin", rows, cols)
+@test reduce(&, m .== view(input, rows, cols))
+
 rm("test.bin")
